f35154e7f6ab54d1362e0cc48cea75518902753f
chunyuan <ae8602563@gmail.com>
2022-06-22 22:43:39 +0800

    Feature: js-cookie for i18n 預設

--- src/components/I18nBtn.vue
+++ src/components/I18nBtn.vue
@@ -12,6 +12,7 @@
         border border-gray-300
         focus:border-blue-700
       "
+      @change="setLang"
     >
       <option value="zh-TW">{{ t("zh-TW") }}</option>
       <option value="en-US">{{ t("en-US") }}</option>
@@ -21,13 +22,19 @@
 
 <script>
 import { useI18n } from "vue-i18n";
+import cookies from 'js-cookie';
+
 export default {
   name: "i18nBtn",
   setup() {
     const { t, locale } = useI18n();
+    const setLang = () => {
+      cookies.set('lang', locale.value)
+    }
     return {
       t,
       locale,
+      setLang
     };
   },
 };
--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -1,10 +1,12 @@
 import { createI18n } from 'vue-i18n/index'
 import zh from '@/language/zh-TW.js'
 import en from '@/language/en-US.js'
+import cookies from 'js-cookie'
 
+const getLang = cookies.get('lang')
 export default createI18n({
   legacy: false,
-  locale: 'zh-TW',
+  locale: getLang || 'zh-TW',
   fallbackLocale: 'zh-TW',
   messages: {
     'zh-TW': zh,
